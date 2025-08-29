9388520f829dd96bf728f13db60db1d858f509b1
Mohit-Sharma-9826 <mohit20064321@gmail.com>
2025-08-29 20:22:38 +0530

    updated password manager

--- TKinter/Password_Manager/main.py
+++ TKinter/Password_Manager/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 import random
+import json
 import pyperclip    # This module is used to copy data in your clipboard.
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -9,7 +10,7 @@ def generate():
                         )
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M','N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    symbols = ['!', '@', '#', '$', '%', '^', '&', '*','_','=', '+', '\\', '|', '<', '>', '/', '?']
+    symbols = ['!', '@', '#', '$', '%', '^', '&', '*','_','=', '+', '|', '<', '>', '/', '?']
 
     n_letter = random.randint(8, 10)
     n_number = random.randint(2, 4)
@@ -36,32 +37,69 @@ def generate():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def pass_saver():
-    alert = messagebox.askyesno(title="Alert!", message="Do you wnat to add these details?")
+    alert = True #messagebox.askyesno(title="Alert!", message="Do you wnat to add these details?")
 
     website = web_name_entry.get()
     name = user_name_entry.get()
     password = passwd_entry.get()
+
+    data = {
+        website: {
+            "email": name,
+            "password": password
+        }
+    }
 
     if len(website) == 0 or len(password) == 0 or len(name) == 0:
         messagebox.showinfo(message="Please fill all details!")
         return
 
     if alert:
-        with open("./TKinter/Password_Manager/PasswordData.txt", "a") as f:
+        try:
+            with open("./TKinter/Password_Manager/PasswordData.json", "r") as f:
+                try:
+                    new_data = json.load(f)
+                    new_data.update(data)
+                except json.JSONDecodeError:
+                    new_data = data
+        except FileNotFoundError:
+            new_data = data
+        
+        with open("./TKinter/Password_Manager/PasswordData.json", "w") as f:
+            json.dump(new_data, f, indent=4)
 
-            data = f"{website} | {name} | {password}\n"
-            f.write(data)
         messagebox.showinfo(title="Infomer", message="Saved Successfully!")
 
     web_name_entry.delete(0, END)
     passwd_entry.delete(0, END)
 
 
+# ---------------------------- Searching ------------------------------- #
+def search():
+    website = web_name_entry.get()
+    
+    user_name_entry.delete(0, END)
+    passwd_entry.delete(0, END)
+
+    try:
+        with open("./TKinter/Password_Manager/PasswordData.json", "r") as f:
+            try:
+                file_data = json.load(f)
+                data = file_data[website]
+                user_name_entry.insert(0, data["email"])
+                passwd_entry.insert(0, data["password"])
+            
+            except KeyError:
+                messagebox.showinfo(message=f"{website} record does not exist!")
+
+    except FileNotFoundError:
+        messagebox.showinfo(message="No records are saved yet!")
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
-
 
 canvas = Canvas(width=200, height=200)
 img = PhotoImage(file="./TKinter/Password_Manager/logo.png")
@@ -71,22 +109,27 @@ canvas.grid(column=1, row=0)
 web_name = Label(text="Website: ",font=("Arial", 15))
 web_name.grid(column=0, row=1)
 
-web_name_entry = Entry(width=45)
-web_name_entry.grid(column=1, row=1, columnspan=2)
+web_name_entry = Entry(width=34)
+web_name_entry.grid(column=1, row=1)
 web_name_entry.focus()
+
+search_btn = Button(text="Search", font=("Arial", 15), bg= "#cfcece", width=12)
+search_btn.grid(column=2, row=1)
 
 user_name = Label(text="Email/Username: ",font=("Arial", 15))
 user_name.grid(column=0, row=2)
 
-user_name_entry = Entry(width=45)
+user_name_entry = Entry(width=60)
 user_name_entry.grid(column=1, row=2, columnspan=2)
 user_name_entry.insert(0, "mohit20064321@gmail.com")
 
 passwd = Label(text="Password: ",font=("Arial", 15))
 passwd.grid(column=0, row=3)
 
-passwd_entry = Entry(width=21)
+passwd_entry = Entry(width=34)
 passwd_entry.grid(column=1, row=3)
+
+search_btn.config(command=search)
 
 gen_btn = Button(text="Generate Password", font=("Arial", 12), bg= "#cfcece", command=generate)
 gen_btn.grid(column=2, row=3)
